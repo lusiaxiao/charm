b7c189cc10e252667cc389447639fcbbcb27feca
Lusia Zhu <849470902@qq.com>
2018-03-15 20:50:11 +0800

    JS基础练习

---
+++ WebContent/js/script.js
@@ -0,0 +1,11 @@
+
+
+function Girl(name,sex,birth){
+    this.name = name;
+    this.sex = sex;
+    this.birth = birth;
+}
+
+var lusia = new Girl('Lusia','femail','1995-06-10');
+var lusia1 = new Girl('Lusia1','femail','1995-06-11');
+console.log(typeof(lusia))
